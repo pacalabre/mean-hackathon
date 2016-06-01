1cf8afefa178275c99d615c0eb80882d8f3f55e5
Paul <pacalabre@gmail.com>
2016-06-01 14:47:28 -0700

    added app.js, controllers.js, and services.js

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+angular.module('hackathonApp', ['appCtrls','ui.router'])
+
+
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/404');
+  console.log("test")
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'app/views/.ejs'
+  })
+  .state('', {
+    url: '/route',
+    templateUrl: 'app/views/.ejs'
+  })
+  .state('login', {
+    url: '/route',
+    templateUrl: 'app/views/.ejs'
+  })
+  .state('cookbook', {
+    url: '/route',
+    templateUrl: 'app/views/.ejs'
+  })
+  .state('recipe-search', {
+    url: 'route',
+    templateUrl: 'app/views/.ejs'
+  })
+  .state('404', {
+    url: '/404',
+    templateUrl: 'app/views/404.ejs'
+  })
+
+
+  $locationProvider.html5Mode(true);
+}]);
